98b101d78feec8da241c7e63f387a5d848c75b1b
LSM-1020 <smmlee@naver.com>
2025-09-05 15:05:28 +0900

    update문

--- src/main/java/com/LSM/jpatest/repository/QuestionRepository.java
+++ src/main/java/com/LSM/jpatest/repository/QuestionRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +30,35 @@ public interface QuestionRepository extends JpaRepository<Questiontbl, Long> { /
 	//제목에 특정문자가 포함되면 조회하기 ->like , 최근글이 위로오도록 정렬
 	public List<Questiontbl> findAllByQtitleLikeOrderByQdateDesc(String keyword); 
 	
-	//SQL문 직접 쓰기
+	//SQL문 직접 쓰기 -> JPA SQL문 (JPQL)
 	@Query("SELECT q FROM Questiontbl q WHERE q.qnum=:qnum") //엔티티 이름을 넣어주기 , *는 적용이 안되서 엔티티를 넣기
 	public Questiontbl findQuestionByQnum(@Param("qnum") Long qnum); //위 qnum을 자동인식하지 않기때문에 @param으로 위 qnum을 지정해줌
+	//SQL문 직접 쓰기->like문
+	@Query("SELECT q FROM Questiontbl q WHERE q.qtitle LIKE %:qtitle%") //엔티티 이름을 넣어주기 , *는 적용이 안되서 엔티티를 넣기
+	public Questiontbl findQuestionByQtitle(@Param("qtitle") String qtitle); //위 qnum을 자동인식하지 않기때문에 @param으로 위 qnum을 지정해줌
+	
+	//질문글 번호가 특정 값보다 큰 질문 글만 조회
+	@Query("SELECT q FROM Questiontbl q WHERE q.qnum >= :number") //엔티티 이름을 넣어주기 , *는 적용이 안되서 엔티티를 넣기
+	public Questiontbl findQuestionByQnumber(@Param("number") Long number); //위 qnum을 자동인식하지 않기때문에 @param으로 위 qnum을 지정해줌
+	
+	
+	//Native SQL문 (오리지널 SQL문) 쓰기
+	@Query(value = "SELECT * FROM jpaquestiontbl WHERE qnum=:qnum", nativeQuery = true)
+	public Questiontbl findQuestionNativeByQnum(@Param("qnum") Long qnum);
+	
+	//기타 JPA문법
+	public boolean existsByQnum(Long qnum); //qnum이 존재하는 번호면 true반환
+	//질문글 번호가 특정 값 보다 큰 질문글만 조회
+	public Questiontbl findByQnumGreaterThanEqual(Long qnum);
+	//글 전체라 List+엔티티 한개면 그냥 엔티티만
+	
+	//질문 내용 업데이트
+	@Modifying
+	@Transactional
+	@Query("UPDATE Questiontbl q SET q.qcontent = :qcontent WHERE q.qnum = :qnum")
+	public int updateQcontentByQnum(@Param("qcontent") String qcontent, @Param("qnum")Long qnum);
+	
+	@Modifying
+	@Query(value = "UPDATE Questiontbl SET qcontent = :qcontent WHERE qnum= :qnum", nativeQuery = true)
+	public int updateNativeQcontentByQnum(@Param("qcontent") String qcontent, @Param("qnum") Long qnum);
 }
--- src/test/java/com/LSM/jpatest/TestQuestion.java
+++ src/test/java/com/LSM/jpatest/TestQuestion.java
@@ -5,110 +5,158 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.LSM.jpatest.dto.QuestionDto;
 import com.LSM.jpatest.entity.Questiontbl;
 import com.LSM.jpatest.repository.QuestionRepository;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestQuestion {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+//	@Test
+//	@DisplayName("질문 등록 테스트")
+//	@Order(1)
+//	public void writeQuestion() {
+////		Questiontbl question = new Questiontbl();
+////		question.setQtitle("두번째 질문");
+////		question.setQcontent("내일은 금요일이 맞나요?");
+//		
+//		QuestionDto questionDto = new QuestionDto();
+//		questionDto.setQtitle("홍길동");
+//		questionDto.setQcontent("홍길동입니다");
+//		Questiontbl question = questionDto.getEntity(); //dto객체-> entity객체로 변환 questiontbl이 엔티티클래스
+//		
+//		//JPA 메소드는 엔티티 객체만 인자값으로 받을수 있다
+//		questionRepository.save(question); //save=insert문 쿼리 실행 , 일반 클래스는 못쓰고 엔티티클래스만 쓸수있음
+//		
+//	}
+//	
+//	@Test
+//	@DisplayName("질문 삭제 테스트")
+//	@Order(2)
+//	public void deleteQuestion() {
+//		
+//		//questionRepository.deleteById(3L); //3번째 레코드+Long=3L
+//		//기본키 qunu=3인 레코드를 찾아서 삭제
+//		
+////		List<Questiontbl> quList = new ArrayList<>();
+////		questionRepository.deleteAll(quList); //엔티티 리스트 삭제(원하는 레코드들만 삭제)
+//		
+//		questionRepository.deleteAllByQtitle("강감찬"); //질문 제목으로 삭제
+//	}
+//	
+//	@Test
+//	@DisplayName("질문 조회 테스트")
+//	@Order(3)
+//	public void searchQuestion() {
+//		
+//		List<Questiontbl> questionAllList = questionRepository.findAll();
+//		//findAll() -> 모든 레코드 가져오기 (select * from jpaquestiontbl)
+//				for(Questiontbl question :questionAllList) {
+//					System.out.println(question.getQnum());
+//					System.out.println(question.getQcontent());
+//					System.out.println(question.getQcontent());
+//					System.out.println(question.getQdate());
+//					System.out.println(question.getUdate());
+//				}
+//				//가장 최근 질문이 위로 오도록 정렬하여 출력
+//				List<Questiontbl> questionOrderList = questionRepository.findAllByOrderByQdateDesc();
+//				for(Questiontbl question :questionOrderList) {
+//					System.out.println(question.getQnum());
+//					System.out.println(question.getQcontent());
+//					System.out.println(question.getQcontent());
+//					System.out.println(question.getQdate());
+//					System.out.println(question.getUdate());
+//	}
+//	}
+//	
+//	@Test
+//	@DisplayName("특정 질문 검색")
+//	@Order(4)
+//	public void searchQuestionByField() {
+//		Optional<Questiontbl> questionOption = questionRepository.findById(4L);
+//		//select * from jpaquestiontbl where qnum=4
+//		
+//		//기본키로 검색했을경우 레코드가 1개 또는 존재하지 않는 경우 발생
+//		//Option<Questiontbl>형태로 반환타입 정해야한다
+//		//isPresent()를 활용하면 Option<Questiontbl>내에 객체의 존재여부를 알수있다
+//		if(questionOption.isPresent()) { //참이면 해당 기본키를 가진 레코드가 존재 ->조회성공
+//			Questiontbl question = questionOption.get();
+//			System.out.println(question.getQtitle());
+//		} else { //해당 기본키를 가진 레코드가 존재하지 않음-> 조회실패
+//			 System.out.println("해당 질문이 존재하지 않습니다.");
+//		}
+//		
+//		//select * from jpaquestiontbl where qnum=1 and qtitle=홍길동
+//		Questiontbl qOptional = questionRepository.findByQnumAndQtitle(13L,"홍길동");
+//		
+//		//질문 제목이 정확히 일치하는 조건으로 조회
+//		 List<Questiontbl> questions = questionRepository.findAllByQtitle("홍길동");
+//		for(Questiontbl question:questions) {
+//			 System.out.println(question.getQtitle());
+//			 System.out.println(question.getQnum());
+//			 System.out.println("========================");
+//		}
+//	
+//		//질문 제목에 특정문자 포함하면 찾는 조건으로 조화 ->like
+//		 List<Questiontbl> likeQuestions = questionRepository.findAllByQtitleLikeOrderByQdateDesc("%질문%");
+//		 for(Questiontbl question:likeQuestions) {
+//			 System.out.println(question.getQtitle());
+//			 System.out.println(question.getQnum());
+//			 System.out.println("========================");
+//		}
+//		//직접 쓴 sql문 (@Query사용) 으로 조회
+//		 Questiontbl questionSql = questionRepository.findQuestionByQnum(4L);
+//		 System.out.println(questionSql.getQnum());
+//		 System.out.println(questionSql.getQtitle());	 
+//	}
 	@Test
-	@DisplayName("질문 등록 테스트")
-	public void writeQuestion() {
-//		Questiontbl question = new Questiontbl();
-//		question.setQtitle("두번째 질문");
-//		question.setQcontent("내일은 금요일이 맞나요?");
+	@DisplayName("특정 질문 내용 업데이트")
+	public void updateQuestion() {
+		int updateResult = questionRepository.updateQcontentByQnum("질문내용을 변경하였습니다23", 14L);
 		
-		QuestionDto questionDto = new QuestionDto();
-		questionDto.setQtitle("홍길동");
-		questionDto.setQcontent("홍길동입니다");
-		Questiontbl question = questionDto.getEntity(); //dto객체-> entity객체로 변환 questiontbl이 엔티티클래스
-		
-		//JPA 메소드는 엔티티 객체만 인자값으로 받을수 있다
-		questionRepository.save(question); //save=insert문 쿼리 실행 , 일반 클래스는 못쓰고 엔티티클래스만 쓸수있음
-		
-	}
-	
-	@Test
-	@DisplayName("질문 삭제 테스트")
-	public void deleteQuestion() {
-		
-		//questionRepository.deleteById(3L); //3번째 레코드+Long=3L
-		//기본키 qunu=3인 레코드를 찾아서 삭제
-		
-//		List<Questiontbl> quList = new ArrayList<>();
-//		questionRepository.deleteAll(quList); //엔티티 리스트 삭제(원하는 레코드들만 삭제)
-		
-		questionRepository.deleteAllByQtitle("강감찬"); //질문 제목으로 삭제
-	}
-	
-	@Test
-	@DisplayName("질문 조회 테스트")
-	public void searchQuestion() {
-		
-		List<Questiontbl> questionAllList = questionRepository.findAll();
-		//findAll() -> 모든 레코드 가져오기 (select * from jpaquestiontbl)
-				for(Questiontbl question :questionAllList) {
-					System.out.println(question.getQnum());
-					System.out.println(question.getQcontent());
-					System.out.println(question.getQcontent());
-					System.out.println(question.getQdate());
-					System.out.println(question.getUdate());
-				}
-				//가장 최근 질문이 위로 오도록 정렬하여 출력
-				List<Questiontbl> questionOrderList = questionRepository.findAllByOrderByQdateDesc();
-				for(Questiontbl question :questionOrderList) {
-					System.out.println(question.getQnum());
-					System.out.println(question.getQcontent());
-					System.out.println(question.getQcontent());
-					System.out.println(question.getQdate());
-					System.out.println(question.getUdate());
-	}
-	}
-	
-	@Test
-	@DisplayName("특정 질문 검색")
-	public void searchQuestionByField() {
-		Optional<Questiontbl> questionOption = questionRepository.findById(4L);
-		//select * from jpaquestiontbl where qnum=4
-		
-		//기본키로 검색했을경우 레코드가 1개 또는 존재하지 않는 경우 발생
-		//Option<Questiontbl>형태로 반환타입 정해야한다
-		//isPresent()를 활용하면 Option<Questiontbl>내에 객체의 존재여부를 알수있다
-		if(questionOption.isPresent()) { //참이면 해당 기본키를 가진 레코드가 존재 ->조회성공
-			Questiontbl question = questionOption.get();
-			System.out.println(question.getQtitle());
-		} else { //해당 기본키를 가진 레코드가 존재하지 않음-> 조회실패
-			 System.out.println("해당 질문이 존재하지 않습니다.");
-		}
-		
-		//select * from jpaquestiontbl where qnum=1 and qtitle=홍길동
-		Questiontbl qOptional = questionRepository.findByQnumAndQtitle(13L,"홍길동");
-		
-		//질문 제목이 정확히 일치하는 조건으로 조회
-		 List<Questiontbl> questions = questionRepository.findAllByQtitle("홍길동");
-		for(Questiontbl question:questions) {
-			 System.out.println(question.getQtitle());
+		Optional<Questiontbl> questionOptional = questionRepository.findById(14L);
+		if(questionOptional.isPresent()) {
+			Questiontbl question =  questionOptional.get();
 			 System.out.println(question.getQnum());
-			 System.out.println("========================");
+			 System.out.println(question.getQcontent());			
+		} else {
+			 System.out.println("조회되지 않았습니다");
 		}
-	
-		//질문 제목에 특정문자 포함하면 찾는 조건으로 조화 ->like
-		 List<Questiontbl> likeQuestions = questionRepository.findAllByQtitleLikeOrderByQdateDesc("%질문%");
-		 for(Questiontbl question:likeQuestions) {
-			 System.out.println(question.getQtitle());
-			 System.out.println(question.getQnum());
-			 System.out.println("========================");
-		}
-		//직접 쓴 sql문 (@Query사용) 으로 조회
-		 Questiontbl questionSql = questionRepository.findQuestionByQnum(4L);
-		 System.out.println(questionSql.getQnum());
-		 System.out.println(questionSql.getQtitle());	 
+		
 	}
+	@Test
+	@DisplayName("Update문->JPA 방법")
+	@Transactional
+	@Rollback(false) //실제 DB에도 반영
+	public void updateJpaQuestion() {
+		Optional<Questiontbl> questionOptional = questionRepository.findById(5L);
+		Questiontbl question = questionOptional.get();
+		
+		System.out.println(question.getQnum());//질문 글 번호
+		System.out.println(question.getQcontent());//질문 글 내용
+		
+		question.setQcontent("질문내용수정!!!!!!!!"); //qcontent 수정(update)
+		
+		Optional<Questiontbl> questionOptional2 = questionRepository.findById(5L);
+		
+		Questiontbl question2 = questionOptional2.get();
+		
+		System.out.println(question2.getQnum());//질문 글 번호
+		System.out.println(question2.getQcontent());//질문 글 내용
+		
+	}
+	
+	
 }
